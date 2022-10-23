a37e171d150c160a0facafc183dd31334e8bad45
gmlwn7245 <gmlwn7245@naver.com>
2022-10-23 21:08:35 +0900

    221023

--- BOJ_SS/boj14502.java
+++ BOJ_SS/boj14502.java
@@ -1,7 +1,126 @@
 package BOJ_SS;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class boj14502 {
+	static class Point{
+		int x, y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	private static int n,m,max=0;
+	private static int[][] map;
+	
+	private static int[] dx = {1,-1,0,0};
+	private static int[] dy = {0,0,1,-1};
+	
+	private static ArrayList<Point> blanks = new ArrayList<>();
+	private static ArrayList<Point> virus = new ArrayList<>();
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
 		
+		map = new int[n][m];
+		
+		// 0빈칸 1벽 2바이러스
+		for(int i=0; i<n; i++) 
+			for(int j=0; j<m; j++) {
+				map[i][j] = sc.nextInt();
+				
+				if(map[i][j]==0)
+					blanks.add(new Point(i,j));
+				else if(map[i][j]==2)
+					virus.add(new Point(i,j));
+			}
+		
+		makeWall(0,-1, new ArrayList<Point>());
+				
+		System.out.println(max);
+	}
+	
+	public static void sepVirus(int[][] newMap) {
+		Queue<Point> q = new LinkedList<>(virus);
+		
+		int[][] setVirusMap = newMap;
+		
+		while(!q.isEmpty()) {
+			Point p = q.poll();
+			setVirusMap[p.x][p.y]=2;
+			
+			for(int i=0; i<4; i++) {
+				int nextX = p.x + dx[i];
+				int nextY = p.y + dy[i];
+				
+				if(!isInArea(nextX, nextY))
+					continue;
+				
+				if(setVirusMap[nextX][nextY]!=0)
+					continue;
+				
+				q.add(new Point(nextX, nextY));
+			}
+		}
+		
+		setMax(setVirusMap);
+	}
+	
+
+	// 벽 만들기
+	public static void makeWall(int dep, int idx, ArrayList<Point> wall){
+		if(dep==3) {
+			int[][] newMap = copyMap();
+			for(Point p : wall)
+				newMap[p.x][p.y]=1;
+			
+			//printMap(newMap);
+			sepVirus(newMap);
+			return ;
+		}
+		
+		for(int i=idx+1; i<blanks.size(); i++) {
+			wall.add(blanks.get(i));
+			makeWall(dep+1, i, wall);
+			wall.remove(dep);
+		}
+	}
+	
+	public static void setMax(int[][] newMap) {
+		int cnt=0;
+		for(int i=0; i<n; i++)
+			for(int j=0; j<m; j++)
+				if(newMap[i][j]==0)
+					cnt++;
+		max = Math.max(cnt, max);
+	}	
+	
+	public static int[][] copyMap(){
+		int[][] newMap = new int[n][m];
+		for(int i=0; i<n; i++)
+			for(int j=0; j<m; j++)
+				newMap[i][j] = map[i][j];
+		
+		return newMap;
+	}
+	
+	public static void printMap(int[][] newMap) {
+		System.out.println("======Print Map======");
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++)
+				System.out.print(newMap[i][j]+" ");
+			System.out.println();
+		}
+			
+	}
+	
+	public static boolean isInArea(int x, int y) {
+		return x<n && x>=0 && y<m && y>=0;
 	}
 }
---
+++ Programmers/KaKao2022_01.java
@@ -0,0 +1,63 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 두 큐 합 같게 만들기
+public class KaKao2022_01 {
+	public static void main(String[] args) {
+		int[] queue1 = {3, 2, 7, 2};
+		int[] queue2 = {4, 6, 5, 1};
+		
+		long sum1 = 0, sum2 = 0;
+		int qLen = queue1.length;
+		
+		ArrayList<Integer> queue = new ArrayList<>();
+		
+		for(int i : queue1) {
+			sum1 += i;
+			queue.add(i);
+		}
+	
+		for(int i : queue2) {
+			sum2 += i;
+			queue.add(i);
+		}
+		
+		if((sum1+sum2)%2!=0)
+			System.out.println("no 1==>-1");
+		
+		else if(sum1 == sum2)
+			System.out.println("no 1==>0");
+					
+		else {
+			int start1=0, start2=qLen, end = queue.size(), changeCnt = 0;
+			boolean isChange = false;
+			while(sum1!=sum2) {
+				if(isChange && start1==0 || start1 == start2) {
+					System.out.println(-1);
+					
+					break;
+				}
+				
+				if(sum1 > sum2) {
+					int n = queue.get(start1);
+					sum1 -= n;
+					sum2 += n;
+					
+					start1 = (start1+1) % end;
+					isChange = true;
+				}else {
+					int n = queue.get(start2);
+					
+					sum2 -= n;
+					sum1 += n;
+					
+					start2 = (start2+1) % end;
+				}
+				changeCnt++;
+			}
+		}
+	}
+}
