43f395806a56e33f0b0d4d7216e8df92c0117b85
gmlwn7245 <gmlwn7245@naver.com>
2022-11-06 23:57:59 +0900

    221106

---
+++ BOJ/boj15486.java
@@ -0,0 +1,76 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj15486 {
+	static class Node {
+		int T, P;
+		public Node(int T, int P) {
+			this.T = T;
+			this.P = P;
+		}
+	}
+	
+	private static int N, maxP=0;
+	private static Node[] node;
+	private static boolean[] isPossible;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		node = new Node[N+1];
+		isPossible = new boolean[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			
+			node[i] = new Node(t,p);
+			if(canDoSchedule(i))
+				isPossible[i] = true;
+		}
+		
+		int firstend = node[1].T;
+		
+		for(int i=1; i<firstend; i++) {
+			dfs(node[i].P,i+1);
+		}
+		
+		bw.write(maxP+"");
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void dfs(int sum, int idx) {
+		if(idx==N+1) {
+			maxP = Math.max(maxP, sum);
+			return ;
+		}
+		
+		for(int i=idx; i<=N; i++) {
+			if(!isPossible[i])
+				continue;
+			
+			if(!canDoSchedule(i))
+				continue;
+			
+			dfs(sum+node[i].P , i+1);
+		}
+		
+		dfs(sum, N+1);
+	}
+	
+	public static boolean canDoSchedule(int n) {
+		return node[n].T + n - 1 <= N;
+	}
+}
