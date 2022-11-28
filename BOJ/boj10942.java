d0f65a204fa394ad3ce422c3a5fc4a42f024e4d6
gmlwn7245 <gmlwn7245@naver.com>
2022-11-28 18:04:34 +0900

    221128

--- BOJ/boj10942.java
+++ BOJ/boj10942.java
@@ -12,25 +12,32 @@ public class boj10942 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		String str = br.readLine().replace(" ", "");
 		
+		int[] nums = new int[N+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
 		
 		int M = Integer.parseInt(br.readLine());
 		for(int i=0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			int S = Integer.parseInt(st.nextToken());
 			int E = Integer.parseInt(st.nextToken());
+			boolean isSame = true;
+			while(S < E) {
+				if(nums[S]!=nums[E]) {
+					isSame = false;
+					break;
+				}
+				S++;E--;
+			}
 			
-			int mid = S+E-1;
-			
-			//String str1, str2;
-			//if((S+E-1)%2==0)
-				//;
-			
-			if(true)
-				bw.write("0\n");
-			else
+			if(isSame)
 				bw.write("1\n");
+			else
+				bw.write("0\n");
 		}
 		
 		bw.flush();
