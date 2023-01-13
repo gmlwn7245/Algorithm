04351fea3bf27327337e96918bcfe148eadd3b34
gmlwn7245 <gmlwn7245@naver.com>
2023-01-13 23:55:20 +0900

    230113

--- BOJ_1000/boj2602.java
+++ BOJ_1000/boj2602.java
@@ -1,56 +1,26 @@
 package BOJ_1000;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class boj2602 {
-	private static int len, ans=0;
-	private static String[] fMap, lMap,str;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		str = br.readLine().split("");
-
-		fMap = br.readLine().split("");
-		lMap = br.readLine().split("");
-		
-		len = fMap.length;
-		dfs(true, 0,0);
-		dfs(false, 0,0);
-		
-		bw.write(ans+"");
-		bw.flush();
-		bw.close();
-	}
-	
-	// Â¦¼ö
-	public static void dfs(boolean isFirst, int idx, int arrIdx) {
-		for(int i=idx; i<len; i++) {
-			if(isFirst) {
-				if(!fMap[i].equals(str[arrIdx]))
-					continue;
-				
-				if(arrIdx==str.length-1)
-					ans++;
-				else
-					dfs(false, i+1, arrIdx+1);
-			}else {
-				if(!lMap[i].equals(str[arrIdx]))
-					continue;
-				
-				if(arrIdx==str.length-1)
-					ans++;
-				else
-					dfs(true, i+1, arrIdx+1);
-			}
-		}
-	}
-	
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        String[] arr = {br.readLine(), br.readLine()};
+        int ans = 0;
+        for (int idx = 0; idx <= 1; idx++) {
+            int[][] cnt = new int[s.length()+1][arr[0].length()+1];
+            Arrays.fill(cnt[0], 1);
+            for (int i = 1; i <= s.length(); i++) {
+                String cur = arr[(i - idx) & 1];
+                for (int j = 1; j <= cur.length(); j++)
+                    cnt[i][j] = cnt[i][j - 1] + (cur.charAt(j - 1) == s.charAt(i - 1) ? cnt[i - 1][j - 1] : 0);
+            }
+            ans += cnt[s.length()][arr[0].length()];
+        }
+        System.out.println(ans);
+    }
 }
