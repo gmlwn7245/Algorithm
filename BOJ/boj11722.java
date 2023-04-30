01b510f6868a89336fed407311041bc57a3a2430
gmlwn7245 <gmlwn7245@naver.com>
2023-04-30 21:09:03 +0900

    230430

---
+++ BOJ/boj11722.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11722 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] score = new int[N];
+		for(int i=0; i<N; i++)
+			score[i]=Integer.parseInt(st.nextToken());
+			
+		int[] dp = new int[N];
+		int ans = 1; dp[0]=1;
+		for(int i=1; i<N; i++) {
+			int now = score[i];
+			int max = 0, maxN = score[i]; dp[i]=1; 
+			for(int j=i-1; j>=0; j--) {
+				int prev = score[j];
+				if(prev > now) {
+					max = Math.max(max, dp[j]);
+				}
+			}
+			
+			dp[i]+=max;
+			ans = Math.max(ans, dp[i]);
+		}
+		
+		System.out.println(ans);
+	}
+}
